8e40fbfb6404de9dd3345eedd06d35fdd2095efc
TomboY90 <rjsahdia3@gmail.com>
2023-03-01 17:26:16 +0900

    feat(add) : player 움직임 구현

--- script.js
+++ script.js
@@ -15,6 +15,11 @@ window.addEventListener('load', function() {
       this.conllisionX = this.game.width * 0.5;
       this.conllisionY = this.game.height * 0.5;
       this.conllisionRadius= 30;
+      this.speedX = 0;
+      this.speedY = 0;
+      this.dx = 0;
+      this.dy = 0;
+      this.speedModifer = 20;
     }
 
     draw(context) {
@@ -25,6 +30,27 @@ window.addEventListener('load', function() {
       context.fill();
       context.restore();
       context.stroke();
+      context.beginPath();
+      context.moveTo(this.conllisionX, this.conllisionY);
+      context.lineTo(this.game.mouse.x, this.game.mouse.y);
+      context.stroke();
+    }
+
+    update() {
+      this.dx = this.game.mouse.x - this.conllisionX;
+      this.dy = this.game.mouse.y - this.conllisionY;
+
+      const distance = Math.hypot(this.dy, this.dx);
+      if (distance > this.speedModifer) {
+        this.speedX = this.dx / distance || 0;
+        this.speedY = this.dy / distance || 0;
+      } else {
+        this.speedX = 0;
+        this.speedY = 0;
+      }
+
+      this.conllisionX += this.speedX * this.speedModifer;
+      this.conllisionY += this.speedY * this.speedModifer;
     }
   }
 
@@ -34,16 +60,46 @@ window.addEventListener('load', function() {
       this.width = this.canvas.width;
       this.height = this.canvas.height;
       this.player = new Player(this);
+
+      this.mouse = {
+        x: this.width * 0.5,
+        y: this.height * 0.5,
+        pressed: false
+      }
+
+      canvas.addEventListener('mousedown', (e) => {
+        this.mouse.x = e.offsetX;
+        this.mouse.y = e.offsetY;
+        this.mouse.pressed = true;
+      })
+      canvas.addEventListener('mouseup', (e) => {
+        this.mouse.x = e.offsetX;
+        this.mouse.y = e.offsetY;
+        this.mouse.pressed = false;
+      })
+      canvas.addEventListener('mousemove', (e) => {
+        if (this.mouse.pressed) {
+          this.mouse.x = e.offsetX;
+          this.mouse.y = e.offsetY;
+        }
+      })
     }
 
     render(context) {
       this.player.draw(context);
+      this.player.update();
     }
   }
 
   const game = new Game(canvas);
-  game.render(ctx)
 
-  function animate() {}
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.render(ctx)
+    requestAnimationFrame(animate);
+  }
+
+  animate();
 })
 
